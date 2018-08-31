e61481216fa0e71cdc482d72d0800c827c864b41
Хельг <xelgvoras@gmail.com>
2018-08-31 13:43:54 +0300

    Рефакторинг главного экрана: содержимое и расположение элементов.
    Удалена стартовая реализация кода для настройки.

--- app/src/main/java/net/victium/xelg/notatry/MainActivity.java
+++ app/src/main/java/net/victium/xelg/notatry/MainActivity.java
@@ -8,46 +8,23 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 import net.victium.xelg.notatry.data.NotATryPreferences;
 
-public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class MainActivity extends AppCompatActivity {
 
+    TextView mFullNameTextView;
     TextView mPersonalInfoTextView;
-
-    String playerName;
-    String playerAge;
-    boolean  playerSide;
+    TextView mMagicPowerTextView;
+    TextView mDefenceTextView;
+    Button mBatleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mPersonalInfoTextView = findViewById(R.id.tv_personal_info);
-
-        playerName = NotATryPreferences.getPreferredPlayerName(this);
-        playerAge = NotATryPreferences.getPreferredPlayerAge(this);
-        playerSide = NotATryPreferences.isLightSide(this);
-        setupPersonalPreferences();
-    }
-
-    private void setupPersonalPreferences() {
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String personalInfoSummary;
-
-        String playerSideString;
-        if (playerSide) {
-            playerSideString = "светлый";
-        } else {
-            playerSideString = "темный";
-        }
-
-        personalInfoSummary = String.format("%s, возраст: %s, %s иной", playerName, playerAge, playerSideString);
-        mPersonalInfoTextView.setText(personalInfoSummary);
-
-        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
     }
 
     @Override
@@ -59,37 +36,11 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (R.id.action_settings == id) {
-            Intent startSettingsActivity = new Intent(this, SettingsActivity.class);
-            startActivity(startSettingsActivity);
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if (key.equals(getString(R.string.pref_full_name_key))) {
-            playerName = sharedPreferences.getString(key, getString(R.string.pref_full_name_default));
-        } else if (key.equals(getString(R.string.pref_age_key))) {
-            playerAge = sharedPreferences.getString(key, getString(R.string.pref_age_default));
-        } else if (key.equals(getString(R.string.pref_side_key))) {
-            String playerSideString = sharedPreferences.getString(key, getString(R.string.pref_side_label_light));
-            if (playerSideString.equals(getString(R.string.pref_side_light_value))) {
-                playerSide = true;
-            } else {
-                playerSide = false;
-            }
-        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        PreferenceManager.getDefaultSharedPreferences(this)
-                .unregisterOnSharedPreferenceChangeListener(this);
     }
 }
